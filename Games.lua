88f67993fc079ad6f41853c7644894f39efedcff
Zyrion Official <zyrionhub.rbx@gmail.com>
2026-08-09 19:58:12 +0700

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,4 @@
+getgenv().Games = {
+    [9910245722] = "https://raw.githubusercontent.com/ZyrionHubOfficial/scripts/refs/heads/main/IronSoul.lua",
+    [9897663111] = "https://raw.githubusercontent.com/ZyrionHubOfficial/scripts/refs/heads/main/BackstreetSurvival.lua",
+}
